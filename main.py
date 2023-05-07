a9777e020d3351ebb1bb37011111698bd20b1658
Om Rachalwar <69252015+OmRachalwar@users.noreply.github.com>
2023-05-07 12:51:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import openai
+openai.api_key = "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"
+complete = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "When do I have to take my medicine"},
+        {"role": "assistant", "content": "After Lunch and After Dinner"},
+        {"role": "user", "content": "Name of the medicine After lunch"}
+    ]
+)
+message = complete.choices[0].message.content
