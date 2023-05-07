2591cf52dea7fc2a296eaaf0967d93ef0a8fca76
Om Rachalwar <69252015+OmRachalwar@users.noreply.github.com>
2023-05-07 12:52:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openai
 openai.api_key = "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"
 complete = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    model="gpt-3.5-turbo-0301",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "When do I have to take my medicine"},
